3ea5e838b2e8693bfbb6b476b5d723fa90d92fff
Sakshamvnm <sakshamdangol851@gmail.com>
2026-01-19 20:12:29 +0545

    thiw is committed

---
+++ gui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+#creating a label widget
+my_label = Label(root,text="Music Player")
+myLabel = Label(root,text="Welcome to the Music Player App")
+#packing it into the screen
+my_label.grid(row=1,column=0)
+myLabel.grid(row=1,column=0)
+
+root.mainloop()
